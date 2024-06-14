7bd8590ae01523289ff5a8217609f38cfb9a835f
rbgkebere <bkebere10@gmail.com>
2024-06-14 18:58:35 +0000

    Getting user data from signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,23 +1,47 @@
 'use client'
 
 import Link from 'next/link'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
-import {axios} from 'axios'
-
+import axios from 'axios'
+import toast from 'react-hot-toast'
+ 
 
 export default function SignUpPage() {
-
+    const router = useRouter()
     const [user, setUser] = useState({
         email:'',
         password:'',
         username:'',
     })
 
+    const [loading, setloading]=useState(false)
+    const [buttonDisabled,setbuttonDisabled]=useState(false)
+    
     const onSignUp = async ()=>{
 
+        try {
+            setloading(true)
+            const response = await axios.post('/api/users/signup',user)
+            console.log('Signup success', response.data)
+            router.push('/login')
+        } catch (error:any) {
+            console.log('Signup failed', error.message)
+            toast.error(error.message)
+        }finally{
+            setloading(false)
+        }
     }
 
+    useEffect(()=>{
+        if (user.email.length>0 && user.username.length>0 &&
+            user.password.length>0
+        ){
+            setbuttonDisabled(false)
+        }else{
+            setbuttonDisabled(true)
+        }
+    },[user])
 
 
 
@@ -25,7 +49,7 @@ export default function SignUpPage() {
     <div className='flex flex-col items-center justify-center 
                     min-h-screen py-2'>
        
-      <h1>Signup</h1>
+      <h1>{loading? 'Loading':'Signup'}</h1>
       <hr />
       <label htmlFor='username'>username</label>
       <input 
@@ -63,7 +87,7 @@ export default function SignUpPage() {
       <button className='p-2 border border-gray-300 rounded-lg mb-4
       focus:outline-none focus:border-gray-600'
       onClick={onSignUp}
-      >Sign up</button>
+      >{buttonDisabled?'Fill the form':'Signup'}</button>
       <Link href='/login'>Login here</Link>
     </div>
   )
